cbfb5652c78e86b13abde19f628c4542d0406cae
austinstutzman <alsghs49@gmail.com>
2023-08-19 12:57:34 -0400

    finishing up some look/feel stuff.
    Messed with color themes a bit but I"m not too worried about that for this project

--- app/src/main/java/com/example/lemonsqueezy/MainActivity.kt
+++ app/src/main/java/com/example/lemonsqueezy/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,7 +30,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonsqueezy.ui.theme.LemonSqueezyTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,46 +47,71 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LemonSqueezeApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-    ) {
-        var status by remember { mutableStateOf(1) }
-        var squeezeCount by remember { mutableStateOf(1) }
-        when (status) {
-            1 -> {
-                SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_tree,
-                    textResourceId = R.string.tap_tree,
-                    onImageClick = {
-                        status++
-                        squeezeCount = (2..4).random()
-                    })
-            }
+fun LemonSqueezeApp() {
 
-            2 -> {
-                SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_squeeze,
-                    textResourceId = R.string.tap_lemon,
-                    onImageClick = {
-                        squeezeCount--
-                        if (squeezeCount == 0) {
-                            status = 3;
-                        }
-                    })
-            }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            var status by remember { mutableStateOf(1) }
+            var squeezeCount by remember { mutableStateOf(1) }
+            when (status) {
+                1 -> {
+                    SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_tree,
+                        textResourceId = R.string.tap_tree,
+                        onImageClick = {
+                            status++
+                            squeezeCount = (2..4).random()
+                        })
+                }
 
-            3 -> {
-                SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_drink,
-                    textResourceId = R.string.tap_full_glass,
-                    onImageClick = {
-                        status++
-                    })
-            }
+                2 -> {
+                    SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_squeeze,
+                        textResourceId = R.string.tap_lemon,
+                        onImageClick = {
+                            squeezeCount--
+                            if (squeezeCount == 0) {
+                                status = 3;
+                            }
+                        })
+                }
 
-            4 -> {
-                SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_restart,
-                    textResourceId = R.string.tap_empty_glass,
-                    onImageClick = { status = 1 })
+                3 -> {
+                    SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_drink,
+                        textResourceId = R.string.tap_full_glass,
+                        onImageClick = {
+                            status++
+                        })
+                }
+
+                4 -> {
+                    SqueezeImageWithStatus(imageResourceId = R.drawable.lemon_restart,
+                        textResourceId = R.string.tap_empty_glass,
+                        onImageClick = { status = 1 })
+                }
             }
         }
     }
@@ -90,13 +126,17 @@ fun SqueezeImageWithStatus(imageResourceId: Int, textResourceId: Int, onImageCli
     ) {
         Button(
             onClick = onImageClick,
-            shape = RoundedCornerShape(5)
+            shape = RoundedCornerShape(5),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
         ) {
             Image(
                 painterResource(id = imageResourceId), null
             )
         }
-        Text(stringResource(textResourceId))
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            stringResource(textResourceId), style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
 
--- app/src/main/java/com/example/lemonsqueezy/ui/theme/Color.kt
+++ app/src/main/java/com/example/lemonsqueezy/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.lemonsqueezy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFF1E769)
+val PurpleGrey80 = Color(0xFF6A6333)
+val Pink80 = Color(0xFFC6EBD3)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val Purple40 = Color(0xFFB6A73A)
+val PurpleGrey40 = Color(0xFF2D2A16)
 val Pink40 = Color(0xFF7D5260)
